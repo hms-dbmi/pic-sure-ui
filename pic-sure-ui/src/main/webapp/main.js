318ef7e5fc56e8309af597d0a47299594b6b25c8
Jason Stedman <jason_stedman@hms.harvard.edu>
2018-04-25 12:43:19 -0400

    added NHANES public user token and better sorting of autocomplete results

--- pic-sure-ui/src/main/webapp/main.js
+++ pic-sure-ui/src/main/webapp/main.js
@@ -19,6 +19,9 @@ require.config({
 
 require(["filter/filter", "output/outputPanel", "bootstrap"],
 		function(filter, output){
+	sessionStorage.token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0fGF2bGJvdEBkYm1pLmhtcy5oYXJ2YXJkLmVkdSIsImVtYWlsIjoiYXZsYm90QGRibWkuaG1zLmhhcnZhcmQuZWR1In0.51TYsm-uw2VtI8aGawdggbGdCSrPJvjtvzafd2Ii9NU";
+	sessionStorage.environment="NHANES";
+
 	console.log("in main");
 	var firstFilter = new filter.View({
 		model : new filter.Model()
@@ -29,4 +32,16 @@ require(["filter/filter", "output/outputPanel", "bootstrap"],
 	var outputPanel = new output.View({});
 	outputPanel.render();
 	$('#query-results').append(outputPanel.$el);
+	$('#modal-save-button').click(function(event){
+		var environment = $("#environment").val();
+		var token = $("#token").val();
+		if(environment && token){
+			sessionStorage.environment = environment;
+			sessionStorage.token = token;			
+		}else{
+			alert("You must set a token and choose an environment. <br><br>" +
+					"For PL environment get a token from PL-Dev. <br>" +
+					"For NHANES environment get a token from NHANES-Prod");
+		}
+	});
 });
