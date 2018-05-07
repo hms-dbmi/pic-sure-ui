450438c2346ff9bb32718ccd646ac37dba6c136d
Jason Stedman <jason_stedman@hms.harvard.edu>
2018-05-07 09:20:04 -0400

    first stab at querying multiple pic-sure instances

--- src/main/webapp/output/outputPanel.js
+++ src/main/webapp/output/outputPanel.js
@@ -1,21 +1,66 @@
-define(["text!output/outputPanel.hbs", "backbone", "handlebars"], 
-	function(outputTemplate, BB, HBS){
+define(["text!output/outputPanel.hbs","picSure/resourceMeta", "picSure/queryCache", "backbone", "handlebars"], 
+	function(outputTemplate, resourceMeta, queryCache, BB, HBS){
 	var outputView = BB.View.extend({
 		initialize: function(){
 			this.template = HBS.compile(outputTemplate);
 		},
+		totalCount: 0,
 		tagName: "div",
-		runQuery: function(){
-			
+		update: function(query){
+			this.totalCount = 0;
+			var view = this;
+			_.each(resourceMeta, function(picsureInstance){
+				var queryCompletionDeferred = $.Deferred();
+				var callbacks = {
+						success: function(id){
+							console.log(id + " SUCCESS");
+							$.ajax({
+								url : picsureInstance.queryResultBasePath + id + "/JSON",
+								success : function(result){
+									console.log(result);
+									$('#patient-count-' + picsureInstance.id).text(result.data.length);
+									this.totalCount += result.data.length;
+									$('#patient-count').text(this.totalCount);
+								}.bind(view),
+								failure : function(data){
+									console.log(data);
+								}
+							});
+
+						},
+						error: function(id){
+							_.each(_.keys(localStorage), function(key){
+								if(localStorage.getItem(key)==id){
+									localStorage.removeItem(key);
+								}
+							});
+							console.log(id + " ERROR");
+						},
+						running: function(id){
+							console.log(id + " STILL RUNNING");
+						}
+				};
+				queryCache.submitQuery(
+						picsureInstance,
+						query,
+						picsureInstance.id,
+						queryCompletionDeferred, 
+						callbacks);
+				$.when(queryCompletionDeferred).then(function(){
+					console.log("deferred resolved");
+				});
+			}.bind(this));		
 		},
 		render: function(){
-			this.$el.html("SPINNING...");
-			setTimeout(function(){
-				this.$el.html(this.template({}));				
-			}.bind(this), 2000);
+			this.$el.html(this.template(resourceMeta));
+			if(this.totalCount == 0){
+				$('#patient-count').text("?");
+			}else{
+				$('#patient-count').text(this.totalCount);
+			}
 		}
 	});
 	return {
-		View : outputView
+		View : new outputView()
 	}
 });
--- src/main/webapp/picSure/resourceMeta.js
+++ src/main/webapp/picSure/resourceMeta.js
@@ -1,9 +1,24 @@
 define([], function(){
-	return { 
-		nhanes : {
+	var picsureInstances = [
+		{
+			id : "NHANES1",
+			name : "NHANES 1",
+			queryPath : "/NHANES/queryService/runQuery?only_count=true",
+			queryStatusBasePath : "/NHANES/resultService/resultStatus/",
+			queryResultBasePath : "/NHANES/resultService/result/"
+		},{
+			id : "NHANES2",
+			name : "NHANES 2",
+			queryPath : "/NHANES/queryService/runQuery?only_count=true",
+			queryStatusBasePath : "/NHANES/resultService/resultStatus/",
+			queryResultBasePath : "/NHANES/resultService/result/"
+		},{
+			id : "NHANES3",
+			name : "NHANES 3",
 			queryPath : "/NHANES/queryService/runQuery?only_count=true",
 			queryStatusBasePath : "/NHANES/resultService/resultStatus/",
 			queryResultBasePath : "/NHANES/resultService/result/"
 		}
-	};
+		];
+	return picsureInstances;
 });
