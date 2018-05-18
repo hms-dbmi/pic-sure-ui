02438213d238a9058c7a8a931109d472ce742164
Jason Stedman <jason_stedman@hms.harvard.edu>
2018-05-18 10:53:05 -0400

    fixed issue where results were being added to all tabs currently rendered, pulled out half of the DOM generation into the template

--- src/main/webapp/filter/searchResults.js
+++ src/main/webapp/filter/searchResults.js
@@ -4,33 +4,17 @@ define(["output/outputPanel","picSure/queryBuilder", "filter/searchResult", "han
 		init : function(data, view, callback){
 			this.searchResultList = [];
             this.searchResultTabs = HBS.compile(searchResultTabsTemplate);
-			this.addSearchResultRow(data, view, callback);
+			this.addSearchResultRows(data, view, callback);
 
 		}
 	};
-    searchResults.addSearchResultRow = function(data, filterView, queryCallback){
-		var categories = [];
+    searchResults.addSearchResultRows = function(data, filterView, queryCallback){
 		var searchResultList = this.searchResultList;
-		var categoryCount = 0;
-        $('.search-tabs', filterView.$el).append(this.searchResultTabs());
+		$('.search-tabs', filterView.$el).append(this.searchResultTabs(_.keys(data)));
 
+		var searchResutlRowViews = [];
 		_.keys(data).forEach((key) => {
-            categories.push(key);
-
-            var classActive = categoryCount == 0 ? 'active' : '';
-
-            var tab = '<li class="' + classActive + '"><a href="#'
-                + key
-                + '" data-toggle="tab" aria-expanded="true">'
-                + key
-                + '</a></li>';
-
-            var tabContent = '<div class="tab-pane ' + classActive + '" id="'+ key + '"></div>';
-
-            $('.nav-pills', filterView.$el).append(tab);
-            $('.tab-content', filterView.$el).append(tabContent);
-
-            _.each(data[key], function(value){
+           _.each(data[key], function(value){
                 var newSearchResultRow = new searchResult.View({
                     queryCallback : queryCallback,
                     model : new searchResult.Model(value),
@@ -39,12 +23,13 @@ define(["output/outputPanel","picSure/queryBuilder", "filter/searchResult", "han
                 newSearchResultRow.render();
                 searchResultList.push(newSearchResultRow);
 
-                $('.tab-pane', filterView.$el).append(newSearchResultRow.$el);
+                $('#'+key+'.tab-pane', filterView.$el).append(newSearchResultRow.$el);
             });
-            categoryCount++;
-    	})
+	    	});
+	    	
+        $("#"+_.first(_.keys(data))).addClass("active");
 
-	}.bind(searchResults);
+    }.bind(searchResults);
     searchResults.runQuery = function(){
         var query = queryBuilder.createQuery(
             _.pluck(this.filters, "model"));
