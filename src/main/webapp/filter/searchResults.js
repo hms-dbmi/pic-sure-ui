6c9b3da1f5d9538b8b2571001f53cc7a646909a4
Jason Stedman <jason_stedman@hms.harvard.edu>
2018-05-18 14:01:17 -0400

    append all categorySearchResultsViews in a single operation to avoid repeated reflows of the layout

--- src/main/webapp/filter/searchResults.js
+++ src/main/webapp/filter/searchResults.js
@@ -5,15 +5,14 @@ define(["output/outputPanel","picSure/queryBuilder", "filter/searchResult", "han
 			this.searchResultList = [];
             this.searchResultTabs = HBS.compile(searchResultTabsTemplate);
 			this.addSearchResultRows(data, view, callback);
-
 		}
 	};
     searchResults.addSearchResultRows = function(data, filterView, queryCallback){
 		var searchResultList = this.searchResultList;
 		$('.search-tabs', filterView.$el).append(this.searchResultTabs(_.keys(data)));
 
-		var searchResutlRowViews = [];
 		_.keys(data).forEach((key) => {
+		   var categorySearchResultViews = [];
            _.each(data[key], function(value){
                 var newSearchResultRow = new searchResult.View({
                     queryCallback : queryCallback,
@@ -22,9 +21,9 @@ define(["output/outputPanel","picSure/queryBuilder", "filter/searchResult", "han
                 });
                 newSearchResultRow.render();
                 searchResultList.push(newSearchResultRow);
-
-                $('#'+key+'.tab-pane', filterView.$el).append(newSearchResultRow.$el);
+                categorySearchResultViews.push(newSearchResultRow);
             });
+           $('#'+key+'.tab-pane', filterView.$el).append(_.pluck(categorySearchResultViews, "$el"));
 	    	});
 	    	
         $("#"+_.first(_.keys(data))).addClass("active");
