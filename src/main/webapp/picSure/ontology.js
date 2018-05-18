eaa6935ce101b1045d35454663ee507ea9fce837
Jason Stedman <jason_stedman@hms.harvard.edu>
2018-05-18 09:27:15 -0400

    fixing sort for search results to be by the index of the search term in the result, and the length of the result as a tie breaker.

--- src/main/webapp/picSure/ontology.js
+++ src/main/webapp/picSure/ontology.js
@@ -1,46 +1,32 @@
 define(["picSure/resourceMeta"], function(resourceMeta){
-	var autocomplete = function(query, done){
+	var mapResponseToResult = function(query, response){
 		var result = {};
+		console.log(response);
+		result.suggestions = response.map(entry => {
+			var puiSegments = entry.pui.split("/");
+			return {
+				value : entry.name,
+				data : entry.pui,
+				category : puiSegments[5],
+				parent : puiSegments[puiSegments.length-3]
+			};
+		}).sort(function(a, b){
+			var indexOfTerm = a.value.toLowerCase().indexOf(query) - b.value.toLowerCase().indexOf(query);
+			var differenceInLength = a.value.length - b.value.length;
+			return indexOfTerm == 0 ? indexOfTerm + differenceInLength : indexOfTerm;
+		});
+		if(result.length > 100){
+			result = result.slice(0,99);
+		}
+		return result;
+	};
+
+	var autocomplete = function(query, done){
 		var lookupDeferred = $.Deferred();
 		return $.ajax({
 			url: window.location.origin + resourceMeta[0].findPath + "?term=%25"+query+"%25",
 			success: function(response){
-				console.log(response);
-				result.suggestions = response.map(entry => {
-					var puiSegments = entry.pui.split("/");
-					return {
-						value : entry.name,
-						data : entry.pui,
-						category : puiSegments[5],
-						parent : puiSegments[puiSegments.length-3]
-					};
-				}).sort(function(a, b){
-					if(a.value.startsWith(query)){
-						if(b.value.startsWith(query)){
-							if(a.value < b.value){
-								return -1;
-							}else{
-								return 1;
-							}
-						}else{
-							return -1;
-						}
-					}else{
-						if(b.value.startsWith(query)){
-							return 1;
-						}else{
-							if(a.value < b.value){
-								return -1;
-							}else{
-								return 1;
-							}
-						}
-					}
-				});
-				if(result.length > 100){
-					result = result.slice(0,99);
-				}
-				done(result);
+				done(mapResponseToResult(query, response));
 			}.bind({done:done}),
 			complete: function(){
 				lookupDeferred.resolve();
