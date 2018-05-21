cbedf479603c00037300e1dfa7b236160778f86e
Jason Stedman <jason_stedman@hms.harvard.edu>
2018-05-21 08:43:59 -0400

    adding jquery as a dependency to main to avoid loading inconsistencies

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -17,7 +17,7 @@ require.config({
 	}
 });
 
-require(["filter/filterList", "output/outputPanel", "bootstrap"],
+require(["filter/filterList", "output/outputPanel", "jquery", "bootstrap"],
 		function(filterList, output){
 	sessionStorage.token="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuYW1kZXRzQGdtYWlsLmNvbSIsImlzcyI6ImJhciIsImV4cCI6MTUyOTA4NzUyMiwiaWF0IjoxNTI2NDk1NTIyLCJqdGkiOiJGb28iLCJlbWFpbCI6Im5hbWRldHNAZ21haWwuY29tIn0.YZPEPAypRojxWPwYKYuuChRxuLrPm_Adt316HC8n9hw";
 	$.ajaxSetup({
