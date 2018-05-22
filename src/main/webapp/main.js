93ab484ef94d15ce66e4ec7dbf133dc029a8ac6c
Jason Stedman <jason_stedman@hms.harvard.edu>
2018-05-22 07:02:53 -0400

    cleaning up duplicate load of jquery and treeview

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -14,11 +14,14 @@ require.config({
 	shim: {
 		"bootstrap": {
 			deps: ["jquery"]
+		},
+		"treeview": {
+			deps:["bootstrap"]
 		}
 	}
 });
 
-require(["filter/filterList", "output/outputPanel", "jquery", "bootstrap"],
+require(["filter/filterList", "output/outputPanel", "treeview"],
 		function(filterList, output){
 	sessionStorage.token="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuYW1kZXRzQGdtYWlsLmNvbSIsImlzcyI6ImJhciIsImV4cCI6MTUyOTA4NzUyMiwiaWF0IjoxNTI2NDk1NTIyLCJqdGkiOiJGb28iLCJlbWFpbCI6Im5hbWRldHNAZ21haWwuY29tIn0.YZPEPAypRojxWPwYKYuuChRxuLrPm_Adt316HC8n9hw";
 	$.ajaxSetup({
