43a4d84fd3e9b5038813428326d61f8618b60106
Jason Stedman <namdets@gmail.com>
2018-05-23 15:48:02 -0400

    refactored out a startup module for testability, also committing file missing from last commit for jasmine support

---
+++ src/test/javascript/startupSpec.js
@@ -0,0 +1,16 @@
+define(["common/startup", "jquery"], function(startup, $){
+	jasmine.pp = function(obj){return JSON.stringify(obj, undefined, 2);};
+	describe("startup", function(){
+		it("sets a token in the jquery ajax", function(){
+			expect($.ajaxSettings.headers.Authorization).toEqual("Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuYW1kZXRzQGdtYWlsLmNvbSIsImlzcyI6ImJhciIsImV4cCI6MTUyOTA4NzUyMiwiaWF0IjoxNTI2NDk1NTIyLCJqdGkiOiJGb28iLCJlbWFpbCI6Im5hbWRldHNAZ21haWwuY29tIn0.YZPEPAypRojxWPwYKYuuChRxuLrPm_Adt316HC8n9hw");
+		});
+		
+		it("appends an empty outputPanel", function(){
+			expect($('#query-results div #patient-count-box')).toBeDefined();
+		});
+		
+		it("initializes the filterList", function(){
+			expect($('#query-builder#query-results')).toBeDefined();
+		});
+	});
+});
