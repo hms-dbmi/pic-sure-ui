eb42ee81d6917ad53531730a43ce060ae60157bf
Jason Stedman <namdets@gmail.com>
2018-06-07 09:30:28 -0400

    refactoring to reduce settings management overhead

--- pic-sure-ui/src/main/webapp/picSure/queryCache.js
+++ pic-sure-ui/src/main/webapp/picSure/queryCache.js
@@ -5,40 +5,44 @@ define(['picSure/ontology', 'jquery','underscore'], function(ontology, $, _){
 	var submitQuery = function(targetSystem, query, displayName, dataCallback){
 		var checkStatus = function(id, stillRunning){
 			setTimeout(function(){
-				$.get(targetSystem.queryStatusBasePath + runningQueryIds[displayName], function(data){
-					switch(data.status){
-					case "RUNNING":
-						// Query is still running so just keep waiting.
-						stillRunning();
-						break;
-					case "AVAILABLE":
-						// Query has completed
-						$.ajax({
-							url : targetSystem.queryResultBasePath + id + "/JSON",
-							success : function(result){
-								dataCallback(result);
-							},
-							failure : function(data){
-								console.log(data);
-							}
-						});
-						break;
-					case "ERROR":
-						// Query failed
-						dataCallback(data);
-						break;
-					default : 
-						console.log("UNKNOWN QUERY STATUS : " + data.status);
-						dataCallback(undefined);
-						break;
-					};
-			});
+				$.ajax(targetSystem.queryStatusBasePath + runningQueryIds[displayName], {
+					success: function(data){
+						switch(data.status){
+						case "RUNNING":
+							// Query is still running so just keep waiting.
+							stillRunning();
+							break;
+						case "AVAILABLE":
+							// Query has completed
+							$.ajax({
+								url : targetSystem.queryResultBasePath + id + "/JSON",
+								success : function(result){
+									dataCallback(result);
+								},
+								failure : function(data){
+									console.log(data);
+								}
+							});
+							break;
+						case "ERROR":
+							// Query failed
+							dataCallback(data);
+							break;
+						default :
+							console.log("UNKNOWN QUERY STATUS : " + data.status);
+							dataCallback(undefined);
+							break;
+						};
+					},
+					headers: {"Authorization": "Bearer " + localStorage.getItem("id_token")}
+				});
 			}, 500);
 		}
 
 		var initiateQuery = function(){
 			$.ajax(targetSystem.queryPath, {
 				data : JSON.stringify(query),
+				headers: {"Authorization": "Bearer " + localStorage.getItem("id_token")},
 				contentType: 'application/json',
 				type: 'POST',
 				success: function(data, status, jqXHR){
--- pic-sure-ui/src/main/webapp/picSure/resourceMeta.js
+++ pic-sure-ui/src/main/webapp/picSure/resourceMeta.js
@@ -1,3 +1,9 @@
-define("text!settings/settings.json", function(settings){
-	return JSON.parse(settings).resources;
+define(["text!settings/settings.json"], function(settings){
+	return JSON.parse(settings).resources.map(resource => Object.assign(resource, {
+		"queryPath" : resource.basePath + "/queryService/runQuery",
+		"queryStatusBasePath" : resource.basePath + "/resultService/resultStatus/",
+		"queryResultBasePath" : resource.basePath + "/resultService/result/",
+		"pathPath" : resource.basePath + "/resourceService/jsonPath",
+		"findPath" : resource.basePath + "/resourceService/find",
+	}));
 });
