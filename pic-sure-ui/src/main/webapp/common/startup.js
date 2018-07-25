8b5a4431e64464466c9c780d807d086ce843e674
Alex Nikitin <aleksandr.nikitin@childrens.harvard.edu>
2018-07-25 14:36:36 -0400

    Handle 401 redirect if unauthorized on primary resource (BCH)

--- pic-sure-ui/src/main/webapp/common/startup.js
+++ pic-sure-ui/src/main/webapp/common/startup.js
@@ -1,6 +1,12 @@
-define(["filter/filterList", "header/header", "output/outputPanel", "jquery", "auth/login", "treeview"],
-		function(filterList, header, output, $, login){
-	console.log("in startup");
+define(["filter/filterList", "header/header", "output/outputPanel", "picSure/resourceMeta", "jquery", "auth/login", "treeview"],
+		function(filterList, header, output, resourceMeta, $, login){
+    console.log("in startup");
+    $(document).ajaxError(function (e, xhr, options) {
+		var isPrimaryResource = options.url.toUpperCase().indexOf(resourceMeta[0].id.toUpperCase()) != -1;
+	    if (xhr.status == 401 && isPrimaryResource){
+		    header.logout();
+		}
+	});
 	if(!localStorage.id_token){
 		login.showLoginPage();
 	}else{
