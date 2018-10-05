dd2be358e9e578e31e2f17940fd652c9687df928
Jason Stedman <namdets@gmail.com>
2018-10-05 09:11:43 -0400

    fixing footer styling

--- pic-sure-ui/src/main/webapp/footer/footer.js
+++ pic-sure-ui/src/main/webapp/footer/footer.js
@@ -1,11 +1,12 @@
-define(["backbone","handlebars", "text!footer/footer.hbs", "overrides/footer"], 
-		function(BB, HBS, login, template, overrides, settings){
+define(["backbone","handlebars", "text!footer/footer.hbs", "overrides/footer", "text!../settings/settings.json"], 
+		function(BB, HBS, template, overrides, settings){
 	var footerView = BB.View.extend({
 		initialize : function(){
 			this.template = HBS.compile(template);
+			this.settings = JSON.parse(settings);
 		},
 		render : typeof overrides.render === 'function' ? overrides.render : function(){
-			this.$el.html(this.template({ footerMessage : settings.footerMessage }));
+			this.$el.html(this.template({ footerMessage : this.settings.footerMessage }));
 		}
 	});
 
