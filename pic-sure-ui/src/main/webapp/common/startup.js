8b29293dab8e7e7f5b5373f17080e5afa5d83c67
Alex Nikitin <aleksandr.nikitin@childrens.harvard.edu>
2019-03-01 13:41:46 -0500

    removed unused auth -> authentication is done through psamaui; updated logout in header;

--- pic-sure-ui/src/main/webapp/common/startup.js
+++ pic-sure-ui/src/main/webapp/common/startup.js
@@ -1,20 +1,45 @@
-define(["filter/filterList", "header/header", "footer/footer", "text!../settings/settings.json", "output/outputPanel", "picSure/resourceMeta", "jquery", "auth/login", "handlebars", "text!common/mainLayout.hbs", "treeview", "common/styles"],
-		function(filterList, header, footer, settings, output, resourceMeta, $, login, HBS, layoutTemplate){
-	return function(){
-		if( (! localStorage.id_token) || (new Date().getTime() - localStorage.expires_at > 0)){
-			login.showLoginPage();
-		}else{
-			$('body').append(HBS.compile(layoutTemplate)(JSON.parse(settings)));
-			var headerView = header.View;
-			headerView.render();
-			$('#header-content').append(headerView.$el);
-			var footerView = footer.View;
-			footerView.render();
-			$('#footer-content').append(footerView.$el);
-			filterList.init();
-			var outputPanel = output.View;
-			outputPanel.render();
-			$('#query-results').append(outputPanel.$el);
-		}
-	}
-});
+define(["filter/filterList", "header/header", "footer/footer", "text!../settings/settings.json", "output/outputPanel", "picSure/resourceMeta", "jquery", "handlebars", "text!common/mainLayout.hbs", "treeview", "common/styles"],
+    function(filterList, header, footer, settings, output, resourceMeta, $, HBS, layoutTemplate){
+        var redirection_url = "/psamaui/login?redirection_url=" + "/picsureui/";
+        return function(){
+            var session = JSON.parse(sessionStorage.getItem("session"));
+            if(!session || !session.token){
+                window.location = redirection_url;
+            }
+            $.ajax({
+                url: window.location.origin + '/BCH/systemService/about',
+                headers: {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem("session")).token},
+                contentType: 'application/json',
+                type:'GET',
+                success: function(){
+                    console.log("login successful");
+                    localStorage.setItem("id_token", JSON.parse(sessionStorage.getItem("session")).token);
+                    $.ajaxSetup({
+                        error: function(event, jqxhr){
+                            console.log(jqxhr);
+                            window.location = redirection_url;
+                        }
+                    });
+                    $('body').append(HBS.compile(layoutTemplate)(JSON.parse(settings)));
+                    var headerView = header.View;
+                    headerView.render();
+                    $('#header-content').append(headerView.$el);
+                    var footerView = footer.View;
+                    footerView.render();
+                    $('#footer-content').append(footerView.$el);
+                    filterList.init();
+                    var outputPanel = output.View;
+                    outputPanel.render();
+                    $('#query-results').append(outputPanel.$el);
+                },
+                error: function(jqXhr){
+                    if(jqXhr.status === 401){
+                        window.location = redirection_url;
+                    }else{
+                        console.log("ERROR in startup.js!!!");
+                    }
+                },
+                dataType: "json"
+            });
+        }
+    });
