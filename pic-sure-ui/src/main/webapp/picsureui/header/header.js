99f35819df839ea102ff765dbf7a30a21175bd8c
Jason Stedman <namdets@gmail.com>
2019-03-06 15:30:48 -0500

    adding admin button and fixing logout

--- pic-sure-ui/src/main/webapp/picsureui/header/header.js
+++ pic-sure-ui/src/main/webapp/picsureui/header/header.js
@@ -1,4 +1,4 @@
-define(["backbone","handlebars", "text!header/header.hbs", "overrides/header", "text!../settings/settings.json"],
+define(["backbone","handlebars", "text!header/header.hbs", "overrides/header", "text!../settings/settings.json"], 
 		function(BB, HBS, template, overrides, settings){
 	var headerView = BB.View.extend({
 		initialize : function(){
@@ -8,16 +8,30 @@ define(["backbone","handlebars", "text!header/header.hbs", "overrides/header", "
 			"click #logout-btn" : "logout"
 		},
 		logout : function(event){
-			localStorage.clear();
 			sessionStorage.clear();
-			window.location = '/';
+			window.location = '/psamaui?redirection_url=/picsureui';
 		}, 
 		render : function(){
 			this.$el.html(this.template({
 				logoPath: (overrides.logoPath 
-					? overrides.logoPath : "/picsureui/images/PrecisionLinkPortal.png"),
+					? overrides.logoPath : "/images/PrecisionLinkPortal.png"),
 				helpLink: JSON.parse(settings).helpLink
 			}));
+			$.ajax({
+				url: window.location.origin + "/picsureauth/user/me",
+				type: 'GET',
+				headers: {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem("session")).token},
+				contentType: 'application/json',
+				success: function(response){
+					if(response.privileges.includes("ADMIN") || response.privileges.includes("SUPER_ADMIN")){
+						$('#admin-btn', this.$el).show();
+					}
+				}.bind(this),
+				error: function(response){
+					console.log("error retrieving user info");
+					console.log(response);
+				}.bind(this)
+			});
 		}
 	});
 
