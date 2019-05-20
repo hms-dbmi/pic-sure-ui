8b4f3f9f5ca913363790363e240a9b2a485f9236
Gabor Korodi <gkorodi@gmail.com>
2019-05-20 14:39:37 -0400

    Add basic tests for new function.

--- pic-sure-ui/src/test/javascript/filterListSpec.js
+++ pic-sure-ui/src/test/javascript/filterListSpec.js
@@ -47,7 +47,40 @@ define(["filter/filterList", "output/outputPanel", "jquery", "text!settings/sett
 				it("is a function", function(){
 					expect(typeof filterList.clearAllQueryFilters).toBe("function")
 				});
-				it(" TODO : needs behavioral tests that require a DOM to check", function(){});
+				it("adds a filter to the list of filters and clears all of them", function(){
+					var filterCountToStartWith = filterList.filters.length;
+					filterList.addFilter();
+					filterList.addFilter();
+					filterList.addFilter();
+					var filterCountBefore = filterList.filters.length;
+
+					filterList.clearAllQueryFilters();
+					var filterCountAfter = filterList.filters.length
+
+					expect(filterCountToStartWith).toBe(1);
+					expect(filterCountBefore).toBe(4);
+					expect(filterCountAfter).toBe(1);
+				});
+				it("clears an initial filter list", function(){
+					var filterCountToStartWith = filterList.filters.length;
+					filterList.clearAllQueryFilters();
+					var filterCountAfter = filterList.filters.length
+					expect(filterCountToStartWith).toBe(1);
+					expect(filterCountAfter).toBe(1);
+					expect(filterList.filters.handle).toBe(undefined);
+				});
+				it("creates an initial filter, runs it and clears it", function(){
+					var filterCountToStartWith = filterList.filters.length;
+					var initialFilter = filterList.filters[0];
+					initialFilter.searchTerm('African American');
+					filterList.clearAllQueryFilters();
+					var filterCountAfter = filterList.filters.length
+					expect(filterCountToStartWith).toBe(1);
+					expect(filterCountAfter).toBe(1);
+					expect(filterList.filters.handle).toBe(undefined);
+				});
+
+				//it(" TODO : needs behavioral tests that require a DOM to check", function(){});
 			});
 		});
 	});
